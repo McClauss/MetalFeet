9b4d4ec6ba95f8aaee7bc73fb46f52dc5bf0e8c2
McClauss <cmhc85@gmail.com>
2022-08-15 16:38:22 -0400

    V12.1
    TDAS

---
+++ Assets/Scripts/TDA.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDA : MonoBehaviour
+{
+    int[] puntaje= new int[3];
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

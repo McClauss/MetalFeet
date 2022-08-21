164e9745d52d7ca99f32a70f2ca8838ff729d8ae
McClauss <cmhc85@gmail.com>
2022-08-21 13:20:53 -0400

    V21
    Se agrega desafio de recorrido TDAs

--- Assets/Scripts/TDA.cs
+++ Assets/Scripts/TDA.cs
@@ -4,32 +4,64 @@ using UnityEngine;
 
 public class TDA : MonoBehaviour
 {
-    //Arrays
+    public GameObject arma;
+    string tag;
+
+    //Arrays para Orbes (Esferas de Salud)
     public GameObject[] orbes;
-    public GameObject[] orbesJugador=new GameObject[10];
+    public GameObject[] orbesJugador=new GameObject[5];
     int i;
     bool recogeOrbe;
+    int contadorOrbes;
 
-    //Lista
+    //Lista para habilidades del Jugador
     public List<string> habilidades=new List<string>();
     public List<string> habilidadesJugador=new List<string>();
     bool banderaCuracion;
     int j;
 
+    //Dictionary para inventario de armas (daño,nombre)
+    public Dictionary<int,string> armas = new Dictionary<int,string>();
+    public Dictionary<int,string> armasJugador = new Dictionary<int,string>();
+    bool recogeArma;
+
+    //Queue(Colas)FIFO
+    Queue apellidos = new Queue();
+
+    //Stack(Pilas)LIFO
+    Stack nombres = new Stack();
+
     void Start()
     {
-        //inicio array
-        i=0;
-        recogeOrbe=false;
-        banderaCuracion=false;
+        //inicio array Orbes
+        IniciaOrbes();
 
         //inicio lista de habilidades
-        habilidades.Add("Curacion");
-        habilidades.Add("Fuerza");
-        habilidades.Add("Estamina");
-        habilidades.Add("Velocidad");
-        j=0;
-        Debug.Log("Habilidades disponibles: "+habilidades.Count);//trae longitud de la lista
+        IniciaHabilidades();
+
+        //inicio diccionaio de armas
+        IniciaArmas();
+
+        /*
+        //Para Llenar la Queue
+        apellidos.Enqueue("Hernandez");
+        apellidos.Enqueue("Calderon");
+        //Para sacar elemento de la Queue
+        apellidos.Dequeue();
+        //Para mostrar elemento de la Queue
+        apellidos.Peak();
+        */
+
+        /*
+        //Para llenar Stack
+        nombres.Push("Claudio");
+        nombres.Push("Marcel");
+        //Para sacar del Stack
+        nombres.Pop();
+        //Para mostrar elemento del Stack
+        nombres.Peak();
+
+        */
     }
 
     void Update()
@@ -37,12 +69,29 @@ public class TDA : MonoBehaviour
         if(recogeOrbe==true){
             SumaOrbe();
         }
+        if(recogeArma==true){
+            SumaArma();
+        }
     }
 
     void OnTriggerEnter(Collider col){
         if(col.transform.gameObject.tag == "Orbe"){
+            tag=col.transform.gameObject.tag;
             recogeOrbe=true;
         }
+        if(col.transform.gameObject.tag == "Hacha Corta"){
+            tag=col.transform.gameObject.tag;
+            recogeArma=true;
+        }
+    }
+
+
+    //Gestión Orbes Salud
+    void IniciaOrbes(){
+        i=0;
+        recogeOrbe=false;
+        banderaCuracion=false;
+        
     }
 
     void SumaOrbe(){
@@ -53,21 +102,61 @@ public class TDA : MonoBehaviour
         if (banderaCuracion==false){
             SumaHabilidad();
         }
-        Debug.Log("Orbe recogido en posición: "+i);
-        recogeOrbe=false;
+            Debug.Log("Orbe recogido en posición: "+i);
+            recogeOrbe=false;
+    }
+
+    //Gestión Habilidades
+    void IniciaHabilidades(){
+        habilidades.Add("Curacion");
+        habilidades.Add("Fuerza");
+        habilidades.Add("Estamina");
+        habilidades.Add("Velocidad");
+        j=0;
+        Debug.Log("Habilidades disponibles: "+habilidades.Count);//trae longitud de la lista
     }
 
     void SumaHabilidad(){
-        /*habilidadesJugador.Add("Curacion");*/
-        habilidadesJugador.Insert(j,"Curacion");//inserta en la lista en X posicion
-        Debug.Log("Habilidad: "+habilidadesJugador[j]+" activada!");
-        j++;
-        banderaCuracion=true;
+        for (int ind=0;ind<habilidadesJugador.Count;ind++){
+            if(habilidadesJugador[ind]=="Curacion"){
+                Debug.Log("Ya tienes la habilidad: "+habilidadesJugador[ind]);
+                banderaCuracion=false;
+            }else{
+                /*habilidadesJugador.Add("Curacion");*/
+                for(int jnd=0;jnd<habilidades.Count;jnd++){
+                    if(habilidades[jnd]=="Curacion"){
+                        habilidadesJugador.Insert(j,habilidades[jnd]);        
+                        Debug.Log("Habilidad: "+habilidadesJugador[j]+" activada!");
+                        j++;
+                        banderaCuracion=false;       
+                    }
+                }
+            }
+        }
     }
     void RestaHabilidad(){
         habilidadesJugador.RemoveAt(j);//para Borrar un dato de la lista en X posición
     }
     void LimpiaHabilidades(){
         habilidadesJugador.Clear();//borra todos los elementos de la vista
+    }
+
+    //Gestión Armas
+    void IniciaArmas(){
+        recogeArma=false;
+        armas.Add(1,"Manos");
+        armasJugador.Add(1,"Manos");
+        armas.Add(2,"Hacha Corta");
+        armas.Add(3,"Espada Corta");
+        armas.Add(4,"Hacha Larga");
+        armas.Add(5,"Mandoble");
+        armas.Add(6,"Arco");
+    }
+
+    void SumaArma(){
+        armasJugador.Add(2,"Hacha Corta");
+        arma.SetActive(false);
+        Debug.Log("Arma: "+armasJugador[2]+" recogida!");
+        recogeArma=false;
     }
 }
